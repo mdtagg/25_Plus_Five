3a9e1823c7b3334ad36d351ac185cbc73fc42266
Mike Tagg <mdtagg91@gmail.com>
2023-06-08 14:25:48 -0700

    iconify added, borders removed

--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -4,12 +4,13 @@ const Counter = () => {
     return (
         <div className="counter-container">
             <div className="counter">
-
+                <p>Session</p>
+                <p>25:00</p>
             </div>
             <div className="controls">
-                <button>Pause</button>
-                <button>Play</button>
-                <button>Reset</button>
+                <button className="pause">Pause</button>
+                <button className="play">Play</button>
+                <button className="reset">Reset</button>
             </div>
         </div>
     )
--- src/components/SessionTimeKeeper/index.tsx
+++ src/components/SessionTimeKeeper/index.tsx
@@ -1,22 +1,29 @@
 import "./index.css"
+import { Icon } from "@iconify/react"
 
 const SessionTimeKeeper = () => {
+
+    const handleClick = () => {
+        console.log('test')
+    }
+
     return (
         <div className="session-container">
             <div className="title">Break Length</div>
-            <div>Up</div>
-            <div>Amount</div>
-            <div>Down</div>
+            <div className="keeper-controls">
+                <Icon 
+                    className="test"
+                    icon="ph:arrow-up-bold" 
+                    onClick={handleClick}/>
+                <div>Amount</div>
+                <Icon 
+                    className="test"
+                    icon="ph:arrow-down-bold"
+                    onClick={handleClick}
+                />
+            </div>
         </div>
-        
     )
 }
 
 export default SessionTimeKeeper
-
-{/* <div className="title">Break Length</div>
-            <div className="controls">
-                <button>Up</button>
-                <p>Amount</p>
-                <button>Down</button>
-            </div> */}
