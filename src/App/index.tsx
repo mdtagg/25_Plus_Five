928c14490770684f2e690ad6c69a5ed9801f740f
Mike Tagg <mdtagg91@gmail.com>
2023-06-08 13:10:55 -0700

    boiler plate for app and component for updating length of each session added

---
+++ src/App/index.tsx
@@ -0,0 +1,23 @@
+import './index.css'
+
+import SessionTimeKeeper from '../components/SessionTimeKeeper'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <div className="one">
+          25 + 5 Clock
+        </div>
+        <SessionTimeKeeper/>
+        <SessionTimeKeeper/>
+        <div>
+
+        </div>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SessionTimeKeeper/index.tsx
@@ -0,0 +1,22 @@
+import "./index.css"
+
+const SessionTimeKeeper = () => {
+    return (
+        <div className="session-container">
+            <div className="title">Break Length</div>
+            <div>Up</div>
+            <div>Amount</div>
+            <div>Down</div>
+        </div>
+        
+    )
+}
+
+export default SessionTimeKeeper
+
+{/* <div className="title">Break Length</div>
+            <div className="controls">
+                <button>Up</button>
+                <p>Amount</p>
+                <button>Down</button>
+            </div> */}
